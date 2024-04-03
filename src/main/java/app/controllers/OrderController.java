27703a3ea42761acd501c9f1d17256584b714b4e
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-04-03 11:52:00 +0200

    - All routes created - All methods created (but empty)

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,14 +1,48 @@
 package app.controllers;
 
 
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 import java.sql.Connection;
 
 public class OrderController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionpool){
-        app.post("addtobasket", ctx)
+        app.post("addtobasket", ctx -> addToBasket(ctx, connectionpool));
+        app.post("myorders", ctx -> viewMyOrders());
+        app.post("basket", ctx -> viewMyBasket());
+        app.post("ordernow", ctx -> placeOrder());
+        app.post("cancelovervieworder", ctx -> cancelOrderInOverview());
+        app.post("orderisready", ctx -> orderReadyToPickup());
+        app.post("rejectorder", ctx -> rejectOrder());
+        app.get("backtoordersite", ctx -> ctx.render("user-frontpage.html"));
+        app.post("cancelorder", ctx -> cancelOrder());
+    }
+
+    private static void addToBasket(Context ctx, ConnectionPool connectionpool) {
+    }
+
+    private static void viewMyOrders() {
+    }
+
+    private static void viewMyBasket() {
+    }
+
+    private static void placeOrder() {
+    }
+
+    private static void cancelOrderInOverview() {
+    }
+
+    private static void orderReadyToPickup() {
+    }
+
+    private static void rejectOrder() {
+    }
+
+    private static void cancelOrder() {
     }
 
 }
