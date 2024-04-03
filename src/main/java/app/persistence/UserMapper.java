4a3e50103279c61fb7b7fb072e64ece1f45a98a1
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-04-03 10:12:20 +0200

    usermapper added

---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,58 @@
+package app.persistence;
+
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper {
+    public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "select * from users where user_email=? and password=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("user_id");
+                String role = rs.getString("user_role");
+                int balance = rs.getInt("user_balance");
+                return new User(id, email, password, role, balance);
+            } else {
+                throw new DatabaseException("Fejl i login. Prøv igen");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Database ERROR", e.getMessage());
+        }
+    }
+
+    public static void createuser(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "insert into users (user_email, password) values (?,?)";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value ")) {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(msg, e.getMessage());
+        }
+    }
+}
