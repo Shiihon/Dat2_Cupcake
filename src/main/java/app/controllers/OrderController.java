6b7d090329aa9e460766b75b3e2c0e7e4f550b39
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-04-03 14:11:09 +0200

    Edits from pull comments

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,20 +1,16 @@
 package app.controllers;
-
-
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
-import java.sql.Connection;
-
 public class OrderController {
 
-    public static void addRoutes(Javalin app, ConnectionPool connectionpool){
-        app.post("addtobasket", ctx -> addToBasket(ctx, connectionpool));
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool){
+        app.post("addtobasket", ctx -> addToBasket(ctx, connectionPool));
         app.post("myorders", ctx -> viewMyOrders());
         app.post("basket", ctx -> viewMyBasket());
         app.post("ordernow", ctx -> placeOrder());
-        app.post("cancelovervieworder", ctx -> cancelOrderInOverview());
+        app.post("cancelorderinorview", ctx -> cancelOrderInOverview());
         app.post("orderisready", ctx -> orderReadyToPickup());
         app.post("rejectorder", ctx -> rejectOrder());
         app.get("backtoordersite", ctx -> ctx.render("user-frontpage.html"));
