619ab4f449bcfb37943ce03eb70802402fd8b12e
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-04-03 11:34:29 +0200

    added connectionpool to main, and added router for UserController.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,10 +1,19 @@
 package app;
 
 import app.config.ThymeleafConfig;
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fourthingsplus";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
 
@@ -16,5 +25,7 @@ public class Main {
         // Routing
 
         app.get("/", ctx -> ctx.render("index.html"));
+
+        UserController.addRoutes(app, connectionPool);
     }
 }
