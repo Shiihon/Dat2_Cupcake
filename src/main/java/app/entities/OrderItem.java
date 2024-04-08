eedc81bb34d50f17472331efcd0e42c8dd82953c
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-04-08 20:50:30 +0200

    Switch cupcake bottom and top

--- src/main/java/app/entities/OrderItem.java
+++ src/main/java/app/entities/OrderItem.java
@@ -3,21 +3,21 @@ package app.entities;
 public class OrderItem {
     private int orderItemId;
     private int orderId;
-    private CupcakePart cupcakeTop;
     private CupcakePart cupcakeBottom;
+    private CupcakePart cupcakeTop;
     private int quantity;
 
-    public OrderItem(int orderItemId, int orderId, CupcakePart cupcakeTop, CupcakePart cupcakeBottom, int quantity) {
+    public OrderItem(int orderItemId, int orderId, CupcakePart cupcakeBottom, CupcakePart cupcakeTop, int quantity) {
         this.orderItemId = orderItemId;
         this.orderId = orderId;
-        this.cupcakeTop = cupcakeTop;
         this.cupcakeBottom = cupcakeBottom;
+        this.cupcakeTop = cupcakeTop;
         this.quantity = quantity;
     }
 
-    public OrderItem(CupcakePart cupcakeTop, CupcakePart cupcakeBottom, int quantity) {
-        this.cupcakeTop = cupcakeTop;
+    public OrderItem(CupcakePart cupcakeBottom, CupcakePart cupcakeTop, int quantity) {
         this.cupcakeBottom = cupcakeBottom;
+        this.cupcakeTop = cupcakeTop;
         this.quantity = quantity;
     }
 
