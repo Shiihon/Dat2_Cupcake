11318b3c4ef9064d685a77e1bab78d7c780402ec
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-04-08 20:28:52 +0200

    added methods

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderMapper {
@@ -38,12 +40,56 @@ public class OrderMapper {
 
     }
 
-    public static Order getOrderById(int orderId, ConnectionPool connectionPool) {
-        return null;
+    public static Order getOrderById(int orderId, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT user_id, order_status, order_timestamp " +
+                " FROM orders WHERE order_id = ?";
+
+        Order order = null;
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int userId = rs.getInt("user_id");
+                String orderStatus = rs.getString("order_status");
+                LocalDateTime timestamp = rs.getTimestamp("order_timestamp").toLocalDateTime();
+                order = new Order(orderId, userId, getAllOrderItems(orderId, connectionPool), orderStatus, timestamp);
+            } else {
+                throw new DatabaseException("The order id doesn't exist");
+            }
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Database Error", e.getMessage());
+        }
+        return order;
     }
 
-    private static List<OrderItem> getAllOrderItems(int orderItem, ConnectionPool connectionPool) {
-        return null;
+
+    private static List<OrderItem> getAllOrderItems(int orderId, ConnectionPool connectionPool) throws SQLException, DatabaseException {
+        List<OrderItem> orderItems = new ArrayList<>();
+
+        String sql = "SELECT order_item_id, cupcake_bottom_id, cupcake_top_id, order_item_quantity FROM order_items WHERE order_id = ?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, orderId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int orderItemId = rs.getInt("order_item_id");
+                int cupcakeBottomId = rs.getInt("cupcake_bottom_id");
+                int cupcakeTopId = rs.getInt("cupcake_top_id");
+                int orderItemQuantity = rs.getInt("order_item_quantity");
+                orderItems.add(new OrderItem(orderItemId, orderId, CupcakeMapper.getCupcakePartById(cupcakeTopId, connectionPool, CupcakePart.Type.TOP), CupcakeMapper.getCupcakePartById(cupcakeBottomId, connectionPool, CupcakePart.Type.BOTTOM), orderItemQuantity));
+            }
+
+        } catch (SQLException e) {
+            throw new DatabaseException("Failed to get all order items");
+        }
+        return orderItems;
     }
 
     private static void createOrderItem(OrderItem orderItem, ConnectionPool connectionPool) throws DatabaseException {
@@ -85,7 +131,7 @@ public class OrderMapper {
     }
 
     private static OrderItem getOrderItemById(int orderItemId, ConnectionPool connectionPool) throws DatabaseException {
-        String sql = "SELECT order_item_id, order_id, cupcake_bottom_id, cupcake_top_id, order_item_quantity " +
+        String sql = "SELECT order_id, cupcake_bottom_id, cupcake_top_id, order_item_quantity " +
                 " FROM order_items WHERE order_id = ?";
 
         OrderItem orderItem = null;
@@ -96,12 +142,11 @@ public class OrderMapper {
         ) {
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                int ordersItemId = rs.getInt("order_item_id");
                 int orderId = rs.getInt("order_id");
                 int cupcakeBottomId = rs.getInt("cupcake_bottom_id");
                 int cupcakeTopId = rs.getInt("cupcake_top_id");
                 int orderItemQuantity = rs.getInt("order_item_quantity");
-                orderItem = new OrderItem(ordersItemId, orderId, CupcakeMapper.getCupcakePartById(cupcakeTopId, connectionPool, CupcakePart.Type.TOP), CupcakeMapper.getCupcakePartById(cupcakeBottomId, connectionPool, CupcakePart.Type.BOTTOM), orderItemQuantity);
+                orderItem = new OrderItem(orderItemId, orderId, CupcakeMapper.getCupcakePartById(cupcakeTopId, connectionPool, CupcakePart.Type.TOP), CupcakeMapper.getCupcakePartById(cupcakeBottomId, connectionPool, CupcakePart.Type.BOTTOM), orderItemQuantity);
             } else {
                 throw new DatabaseException("The order id doesn't exist");
             }
