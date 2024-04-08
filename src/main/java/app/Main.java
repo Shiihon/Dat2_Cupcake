cff41cf8dacc130e022a88c42e4fa65afc0724d8
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-04-08 12:19:14 +0200

    space deleted

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,6 @@ public class Main {
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake";
-
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args) {
